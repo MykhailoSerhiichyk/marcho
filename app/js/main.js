241a3f198def51ab4f682530da5cd1f43ec78443
StreetDream <mishkasaltovskiy@gmail.com>
2021-08-12 15:58:57 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,13 @@ $(function(){
         autoplaySpeed: 2000,
     });
 
+    $(".star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true,
+      });
+
 
 
 
